3f78db48c73406f2b6fc20c28db2f55026c49ba6
Alexandru Florin Patruta <alex.patruta@gmail.com>
2016-05-10 15:28:13 +0300

    Made F4 run more steps.
    Experimented with a high step count, but a small time between steps.
    Time: 0.005 seconds
    Steps: 1 million
    Total time: 5000 seconds -> 83.(33) minutes -> 1.38 hours

--- Multiprocess/main.py
+++ Multiprocess/main.py
@@ -5,8 +5,8 @@ processTestObject = ProcessTest()
 
 p1 = mp.Process(target=processTestObject.F1)
 p2 = mp.Process(target=processTestObject.F2)
-p3 = mp.Process(target=processTestObject.F3, args=(10,))
-p4 = mp.Process(target=processTestObject.F4)
+p3 = mp.Process(target=processTestObject.F3, args=(10, 0.001))
+p4 = mp.Process(target=processTestObject.F4, args=(0.005, 1000000))
 
 if __name__ == '__main__':
     print("INFO | Starting main application")
